04d64a73dee64945a5c0a98279b075ce52773247
AVYA <136340837+Blackpython7@users.noreply.github.com>
2025-05-26 11:35:48 +0530

    Update start.py

--- EsproMusic/utils/inline/start.py
+++ EsproMusic/utils/inline/start.py
@@ -25,7 +25,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/EcZade),
             InlineKeyboardButton(text=_["S_B_12"], callback_data="LG"),
         ],
         [
